f39ab4e0752b6c4b7247a103b39ccb731df53a9a
Tejeshwer Singh Sachdeva <tejeshwersingh2504@gmail.com>
2022-06-27 15:41:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+  return (
+    <>
+    
+    </>
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
